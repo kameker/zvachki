adbbe60edcd7a3fab108dfc20213bbc9398f8252
kameker <vsrnpra2@gmail.com>
2024-10-30 18:55:32 +0300

    pr10

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
         //System.out.printf("%.06f%n", pr26.solve(1.2,4));
-        pr10.printFigure(6);
+        pr10.printFigure(5);
     }
 }
--- src/pr10.java
+++ src/pr10.java
@@ -1,61 +1,63 @@
 public class pr10 {
-    public static void zv(int h){
-        for (int i = 0 ; i < h;i++){
+    public static void zv(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print("*");
         }
     }
-    public static void tire(int h){
-        for (int i = 0 ; i < h;i++){
+
+    public static void tire(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print("-");
         }
     }
-    public static void slash(int h){
-        for (int i = 0 ; i < h;i++){
+
+    public static void slash(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print("/");
         }
     }
-    public static void reslash(int h){
-        for (int i = 0 ; i < h;i++){
+
+    public static void reslash(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print("\\");
         }
     }
-    public static void ver(int h){
-        for (int i = 0 ; i < h;i++){
+
+    public static void ver(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print("|");
         }
     }
-    public static void space(int h){
-        for (int i = 0 ; i < h;i++){
+
+    public static void space(int h) {
+        for (int i = 0; i < h; i++) {
             System.out.print(" ");
         }
     }
-    public static void printFigure(int h){
-        int m = 2;
+
+    public static void printFigure(int h) {
+        int m = 1;
         int g = 0;
-        for (int i = 0 ; i < h * 2 - 1;i++){
-            if (i == 0){
+        for (int i = 0; i < h; i++) {
+            if (i == 0) {
                 space(h - 1);
                 zv(1);
-            }
-            else if (i % 2 != 0){
-                System.out.print(" ");
-            }
-            else if (i % 2 == 0){
+            } else if (i == h-1) {
+                zv(1);
+                tire(h - 2);
+                zv(1);
+                tire(h - 2);
+                zv(1);
+            } else {
                 space(h - m);
                 slash(1);
                 space(g);
                 ver(1);
                 space(g);
                 reslash(1);
+                g++;
             }
-            else if (i == h * 2){
-                zv(1);
-                tire(h-2);
-                zv(1);
-                tire(h-2);
-                zv(1);
-            }
-            g++;
+
             m++;
             System.out.println();
         }
